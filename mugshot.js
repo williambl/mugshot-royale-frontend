bbc7bee17e4b67a0a50ee7a671238d40c468e89f
William Bradford Larcombe <william@williambl.com>
2018-07-19 23:27:35 +0100

    Upload now goes to the backend

--- mugshot.js
+++ mugshot.js
@@ -3,7 +3,7 @@ $(function(){
         if ($("#photo").prop("files").length) {
             $.ajax({
                 type: 'POST',
-                url: 'https://httpbin.org/post?player='+$("#player").val(),
+                url: 'http://127.0.0.1:5000/upload?player='+$("#player").val(),
                 data: $("#photo").prop('files')[0],
                 processData: false,
                 contentType: $("#photo").prop('files')[0].type
