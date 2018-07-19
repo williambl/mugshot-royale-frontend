c81879cd6c08d76479143fa76fde168e13c4603a
William Bradford Larcombe <william@williambl.com>
2018-07-19 21:24:11 +0100

    File is now sent with correct content-type and only if it exists

--- mugshot.js
+++ mugshot.js
@@ -1,11 +1,13 @@
 $(function(){
     $("#upload").click(function(){
-        $.ajax({
-            type: 'POST',
-            url: 'https://httpbin.org/post',
-            data: $("#photo").prop('files')[0],
-            processData: false,
-            contentType: false
-        });
+        if ($("#photo").prop("files").length) {
+            $.ajax({
+                type: 'POST',
+                url: 'https://httpbin.org/post',
+                data: $("#photo").prop('files')[0],
+                processData: false,
+                contentType: $("#photo").prop('files')[0].type
+            });
+        }
     })
 })
