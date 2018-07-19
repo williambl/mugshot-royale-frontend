b74b13160779ff28936d2014251c93bacc1b733d
William Bradford Larcombe <william@williambl.com>
2018-07-19 21:30:40 +0100

    Player is now sent in the url

--- mugshot.js
+++ mugshot.js
@@ -3,7 +3,7 @@ $(function(){
         if ($("#photo").prop("files").length) {
             $.ajax({
                 type: 'POST',
-                url: 'https://httpbin.org/post',
+                url: 'https://httpbin.org/post?player='+$("#player").val(),
                 data: $("#photo").prop('files')[0],
                 processData: false,
                 contentType: $("#photo").prop('files')[0].type
