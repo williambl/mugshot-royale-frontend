8e054565a79a54251220f36c11f2e689425fa5fb
William Bradford Larcombe <william@williambl.com>
2018-07-19 21:06:02 +0100

    Added a test post request

--- mugshot.js
+++ mugshot.js
@@ -1,3 +1,11 @@
 $(function(){
-    $("h1.title").html("testing jquery!")
+    $("#upload").click(function(){
+        $.ajax({
+            type: 'POST',
+            url: 'https://httpbin.org/post',
+            data: $("#photo").prop('files')[0],
+            processData: false,
+            contentType: false
+        });
+    })
 })
