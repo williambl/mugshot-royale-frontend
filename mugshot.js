36c1af7604b381feca5b5ebf6774b6716a829b54
williambl <william@williambl.com>
2018-07-20 23:16:45 +0100

    Increased max zoom

--- mugshot.js
+++ mugshot.js
@@ -41,7 +41,7 @@ $(function(){
     var map = L.map("map").setView([51.505, -0.09], 13);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
-            maxZoom: 12,
+            maxZoom: 20,
     }).addTo(map);
 
     var safeZone = L.circle([51.508, -0.11], {
